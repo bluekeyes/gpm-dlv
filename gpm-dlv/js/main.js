555ffcfadb60569cf2e6016126ffc5a407538af8
Billy Keyes <bluekeyes@gmail.com>
2015-05-17 20:15:29 -0700

    Fix extension after recent Play Music redesign
    Simplify dynamic element detection by using setInterval, since the
    MutationObserver was missing the necessary update and I don't want to
    figure out why.

--- gpm-dlv/js/main.js
+++ gpm-dlv/js/main.js
@@ -1,38 +1,27 @@
 (function() {
 
-    function findTarget(element) {
-        if (element.nodeType === 1) {
-            var candidates = element.querySelectorAll('.nav-item-container');
-            for (var i = 0; i < candidates.length; i++) {
-                if (candidates[i].textContent == 'My Library') {
-                    return candidates[i];
-                }
+    function findMyLibraryLink() {
+        var candidates = document.querySelectorAll('.nav-item-container');
+        for (var i = 0; i < candidates.length; i++) {
+            if (candidates[i].textContent === 'My Library') {
+                return candidates[i];
             }
         }
     }
 
-    function modifyTarget(element) {
+    function setViewType(element) {
         chrome.storage.sync.get({'gpmDefaultLibraryView': 'albums'}, function(item) {
             element.setAttribute('data-type', item.gpmDefaultLibraryView);
         });
     }
 
-    var observer = new MutationObserver(function(mutations, observer) {
-        mutations.forEach(function(mutation) {
-            for (var i = 0; i < mutation.addedNodes.length; i++) {
-                var element = findTarget(mutation.addedNodes[i]);
-                if (element) {
-                    modifyTarget(element);
-                    observer.disconnect();
-                }
-            }
-        });
-    });
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-        attributes: false,
-        characterData: false
-    });
 
+    var intervalId;
+    intervalId = setInterval(function () {
+        var element = findMyLibraryLink();
+        if (element) {
+            clearInterval(intervalId);
+            setViewType(element);
+        }
+    }, 250);
 })();
